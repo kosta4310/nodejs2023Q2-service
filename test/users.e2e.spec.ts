04885c77c8b511b2914626e17f33f6e1394604a9
Pavlov Kostyantyn <realanka77@gmail.com>
2023-07-27 22:30:36 +0300

    feat: all user tests are done

--- test/users.e2e.spec.ts
+++ test/users.e2e.spec.ts
@@ -7,7 +7,6 @@ import {
   removeTokenUser,
 } from './utils';
 import { usersRoutes } from './endpoints';
-import { response } from 'express';
 
 const createUserDto = {
   login: 'TEST_LOGIN',
@@ -70,7 +69,6 @@ describe('Users (e2e)', () => {
       const cleanupResponse = await unauthorizedRequest
         .delete(usersRoutes.delete(id))
         .set(commonHeaders);
-      console.log(cleanupResponse);
 
       expect(cleanupResponse.statusCode).toBe(StatusCodes.NO_CONTENT);
     });
@@ -146,133 +144,133 @@ describe('Users (e2e)', () => {
     });
   });
 
-  // describe('PUT', () => {
-  //   it('should correctly update user password match', async () => {
-  //     const creationResponse = await unauthorizedRequest
-  //       .post(usersRoutes.create)
-  //       .set(commonHeaders)
-  //       .send(createUserDto);
+  describe('PUT', () => {
+    it('should correctly update user password match', async () => {
+      const creationResponse = await unauthorizedRequest
+        .post(usersRoutes.create)
+        .set(commonHeaders)
+        .send(createUserDto);
 
-  //     const { id: createdId } = creationResponse.body;
+      const { id: createdId } = creationResponse.body;
 
-  //     expect(creationResponse.status).toBe(StatusCodes.CREATED);
+      expect(creationResponse.status).toBe(StatusCodes.CREATED);
 
-  //     const updateResponse = await unauthorizedRequest
-  //       .put(usersRoutes.update(createdId))
-  //       .set(commonHeaders)
-  //       .send({
-  //         oldPassword: createUserDto.password,
-  //         newPassword: 'NEW_PASSWORD',
-  //       });
+      const updateResponse = await unauthorizedRequest
+        .put(usersRoutes.update(createdId))
+        .set(commonHeaders)
+        .send({
+          oldPassword: createUserDto.password,
+          newPassword: 'NEW_PASSWORD',
+        });
 
-  //     expect(updateResponse.statusCode).toBe(StatusCodes.OK);
+      expect(updateResponse.statusCode).toBe(StatusCodes.OK);
 
-  //     const {
-  //       id: updatedId,
-  //       version,
-  //       login,
-  //       createdAt,
-  //       updatedAt,
-  //     } = updateResponse.body;
+      const {
+        id: updatedId,
+        version,
+        login,
+        createdAt,
+        updatedAt,
+      } = updateResponse.body;
 
-  //     expect(login).toBe(createUserDto.login);
-  //     expect(updateResponse.body).not.toHaveProperty('password');
-  //     expect(validate(updatedId)).toBe(true);
-  //     expect(createdId).toBe(updatedId);
-  //     expect(version).toBe(2);
-  //     expect(typeof createdAt).toBe('number');
-  //     expect(typeof updatedAt).toBe('number');
-  //     expect(createdAt === updatedAt).toBe(false);
+      expect(login).toBe(createUserDto.login);
+      expect(updateResponse.body).not.toHaveProperty('password');
+      expect(validate(updatedId)).toBe(true);
+      expect(createdId).toBe(updatedId);
+      expect(version).toBe(2);
+      expect(typeof createdAt).toBe('number');
+      expect(typeof updatedAt).toBe('number');
+      expect(createdAt === updatedAt).toBe(false);
 
-  //     const updateResponse2 = await unauthorizedRequest
-  //       .put(usersRoutes.update(createdId))
-  //       .set(commonHeaders)
-  //       .send({
-  //         oldPassword: createUserDto.password,
-  //         newPassword: 'NEW_PASSWORD',
-  //       });
+      const updateResponse2 = await unauthorizedRequest
+        .put(usersRoutes.update(createdId))
+        .set(commonHeaders)
+        .send({
+          oldPassword: createUserDto.password,
+          newPassword: 'NEW_PASSWORD',
+        });
 
-  //     expect(updateResponse2.statusCode).toBe(StatusCodes.FORBIDDEN);
+      expect(updateResponse2.statusCode).toBe(StatusCodes.FORBIDDEN);
 
-  //     const cleanupResponse = await unauthorizedRequest
-  //       .delete(usersRoutes.delete(createdId))
-  //       .set(commonHeaders);
+      const cleanupResponse = await unauthorizedRequest
+        .delete(usersRoutes.delete(createdId))
+        .set(commonHeaders);
 
-  //     expect(cleanupResponse.statusCode).toBe(StatusCodes.NO_CONTENT);
-  //   });
+      expect(cleanupResponse.statusCode).toBe(StatusCodes.NO_CONTENT);
+    });
 
-  //   it('should respond with BAD_REQUEST status code in case of invalid id', async () => {
-  //     const response = await unauthorizedRequest
-  //       .put(usersRoutes.update('some-invalid-id'))
-  //       .set(commonHeaders)
-  //       .send({
-  //         oldPassword: 'test',
-  //         newPassword: 'fake',
-  //       });
+    it('should respond with BAD_REQUEST status code in case of invalid id', async () => {
+      const response = await unauthorizedRequest
+        .put(usersRoutes.update('some-invalid-id'))
+        .set(commonHeaders)
+        .send({
+          oldPassword: 'test',
+          newPassword: 'fake',
+        });
 
-  //     expect(response.status).toBe(StatusCodes.BAD_REQUEST);
-  //   });
+      expect(response.status).toBe(StatusCodes.BAD_REQUEST);
+    });
 
-  //   it('should respond with BAD_REQUEST status code in case of invalid dto', async () => {
-  //     const response = await unauthorizedRequest
-  //       .put(usersRoutes.update(randomUUID))
-  //       .set(commonHeaders)
-  //       .send({});
+    it('should respond with BAD_REQUEST status code in case of invalid dto', async () => {
+      const response = await unauthorizedRequest
+        .put(usersRoutes.update(randomUUID))
+        .set(commonHeaders)
+        .send({});
 
-  //     expect(response.status).toBe(StatusCodes.BAD_REQUEST);
-  //   });
+      expect(response.status).toBe(StatusCodes.BAD_REQUEST);
+    });
 
-  //   it("should respond with NOT_FOUND status code in case if user doesn't exist", async () => {
-  //     const response = await unauthorizedRequest
-  //       .put(usersRoutes.update(randomUUID))
-  //       .set(commonHeaders)
-  //       .send({
-  //         oldPassword: 'test',
-  //         newPassword: 'fake',
-  //       });
+    it("should respond with NOT_FOUND status code in case if user doesn't exist", async () => {
+      const response = await unauthorizedRequest
+        .put(usersRoutes.update(randomUUID))
+        .set(commonHeaders)
+        .send({
+          oldPassword: 'test',
+          newPassword: 'fake',
+        });
 
-  //     expect(response.status).toBe(StatusCodes.NOT_FOUND);
-  //   });
-  // });
+      expect(response.status).toBe(StatusCodes.NOT_FOUND);
+    });
+  });
 
-  // describe('DELETE', () => {
-  //   it('should correctly delete user', async () => {
-  //     const response = await unauthorizedRequest
-  //       .post(usersRoutes.create)
-  //       .set(commonHeaders)
-  //       .send(createUserDto);
+  describe('DELETE', () => {
+    it('should correctly delete user', async () => {
+      const response = await unauthorizedRequest
+        .post(usersRoutes.create)
+        .set(commonHeaders)
+        .send(createUserDto);
 
-  //     const { id } = response.body;
+      const { id } = response.body;
 
-  //     expect(response.status).toBe(StatusCodes.CREATED);
+      expect(response.status).toBe(StatusCodes.CREATED);
 
-  //     const cleanupResponse = await unauthorizedRequest
-  //       .delete(usersRoutes.delete(id))
-  //       .set(commonHeaders);
+      const cleanupResponse = await unauthorizedRequest
+        .delete(usersRoutes.delete(id))
+        .set(commonHeaders);
 
-  //     expect(cleanupResponse.statusCode).toBe(StatusCodes.NO_CONTENT);
+      expect(cleanupResponse.statusCode).toBe(StatusCodes.NO_CONTENT);
 
-  //     const searchResponse = await unauthorizedRequest
-  //       .get(usersRoutes.getById(id))
-  //       .set(commonHeaders);
+      const searchResponse = await unauthorizedRequest
+        .get(usersRoutes.getById(id))
+        .set(commonHeaders);
 
-  //     expect(searchResponse.statusCode).toBe(StatusCodes.NOT_FOUND);
-  //   });
+      expect(searchResponse.statusCode).toBe(StatusCodes.NOT_FOUND);
+    });
 
-  //   it('should respond with BAD_REQUEST status code in case of invalid id', async () => {
-  //     const response = await unauthorizedRequest
-  //       .delete(usersRoutes.delete('some-invalid-id'))
-  //       .set(commonHeaders);
+    it('should respond with BAD_REQUEST status code in case of invalid id', async () => {
+      const response = await unauthorizedRequest
+        .delete(usersRoutes.delete('some-invalid-id'))
+        .set(commonHeaders);
 
-  //     expect(response.status).toBe(StatusCodes.BAD_REQUEST);
-  //   });
+      expect(response.status).toBe(StatusCodes.BAD_REQUEST);
+    });
 
-  //   it("should respond with NOT_FOUND status code in case if user doesn't exist", async () => {
-  //     const response = await unauthorizedRequest
-  //       .delete(usersRoutes.delete(randomUUID))
-  //       .set(commonHeaders);
+    it("should respond with NOT_FOUND status code in case if user doesn't exist", async () => {
+      const response = await unauthorizedRequest
+        .delete(usersRoutes.delete(randomUUID))
+        .set(commonHeaders);
 
-  //     expect(response.status).toBe(StatusCodes.NOT_FOUND);
-  //   });
-  // });
+      expect(response.status).toBe(StatusCodes.NOT_FOUND);
+    });
+  });
 });
