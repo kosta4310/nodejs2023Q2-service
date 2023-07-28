4f54dd8bf110ba4cceb4795997d4818f326e5186
Pavlov Kostyantyn <realanka77@gmail.com>
2023-07-28 10:27:50 +0300

    refactor: return app service and controller back

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './entities/user/user.module';
 import { DbModule } from './db/db.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [UserModule, DbModule],
+  providers: [AppService],
+  controllers: [AppController],
 })
 export class AppModule {}
