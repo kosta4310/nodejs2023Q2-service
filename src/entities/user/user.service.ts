cec4ddcafcf4fc18c1575b755a5096409dce1551
Pavlov Kostyantyn <realanka77@gmail.com>
2023-08-04 22:53:52 +0300

    feat: add prisma service

--- src/entities/user/user.service.ts
+++ src/entities/user/user.service.ts
@@ -2,20 +2,33 @@ import { HttpException, Injectable } from '@nestjs/common';
 import { DbUserService } from 'src/db/dbUser.service';
 import { CreateUserDto, UpdatePasswordDto } from './interface';
 import { getHashedPassword } from './utils/getHash';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class UserService {
-  constructor(private dbUser: DbUserService) {}
+  constructor(private dbUser: DbUserService, private prisma: PrismaService) {}
 
   async getAllUsers() {
-    return await this.dbUser.findMany();
+    const users = await this.prisma.user.findMany();
+
+    return users.map((user) => ({
+      ...user,
+      createdAt: new Date(user.createdAt).valueOf(),
+      updatedAt: new Date(user.updatedAt).valueOf(),
+    }));
+    // return await this.dbUser.findMany();
   }
 
   async getUserById({ id }) {
-    const user = await this.dbUser.findUnique({ id });
+    const user = await this.prisma.user.findUnique({ where: { id } });
+    // const user = await this.dbUser.findUnique({ id });
 
     if (user) {
-      return user;
+      return {
+        ...user,
+        createdAt: new Date(user.createdAt).valueOf(),
+        updatedAt: new Date(user.updatedAt).valueOf(),
+      };
     }
 
     throw new HttpException(`Record with id === ${id} doesn't exist`, 404);
@@ -24,11 +37,19 @@ export class UserService {
   async createUser(dto: CreateUserDto) {
     const hashedPassword = getHashedPassword(dto.password);
     dto.password = hashedPassword;
-    return await this.dbUser.create({ data: dto });
+    const user = await this.prisma.user.create({ data: dto });
+
+    return {
+      ...user,
+      createdAt: new Date(user.createdAt).valueOf(),
+      updatedAt: new Date(user.updatedAt).valueOf(),
+    };
+    // return await this.dbUser.create({ data: dto });
   }
 
   async updatePassword(dto: UpdatePasswordDto, id: string) {
-    const user = await this.dbUser.findUnique({ id });
+    const user = await this.prisma.user.findUnique({ where: { id } });
+    // const user = await this.dbUser.findUnique({ id });
 
     const oldHashedPassword = getHashedPassword(dto.oldPassword);
 
@@ -37,7 +58,17 @@ export class UserService {
         const newHashedPassword = getHashedPassword(dto.newPassword);
         dto.newPassword = newHashedPassword;
 
-        return await this.dbUser.update({ data: dto, id });
+        const user = await this.prisma.user.update({
+          data: dto,
+          where: { id },
+        });
+
+        return {
+          ...user,
+          createdAt: new Date(user.createdAt).valueOf(),
+          updatedAt: new Date(user.updatedAt).valueOf(),
+        };
+        // return await this.dbUser.update({ data: dto, id });
       }
       throw new HttpException(`OldPassword is wrong`, 403);
     }
@@ -45,9 +76,14 @@ export class UserService {
   }
 
   async deleteUser(id: string) {
-    const user = await this.dbUser.delete({ id });
+    const user = await this.prisma.user.delete({ where: { id } });
+    // const user = await this.dbUser.delete({ id });
     if (user) {
-      return user;
+      return {
+        ...user,
+        createdAt: new Date(user.createdAt).valueOf(),
+        updatedAt: new Date(user.updatedAt).valueOf(),
+      };
     }
     throw new HttpException(`Record with id === ${id} doesn't exist`, 404);
   }
