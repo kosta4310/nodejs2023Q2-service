602ab911a274776102ea43eb6492c5a8c3f6f341
Pavlov Kostyantyn <realanka77@gmail.com>
2023-08-19 18:25:39 +0300

    feat: add passport

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import { SwaggerModule } from '@nestjs/swagger';
 const PORT = process.env.PORT || 4000;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
 
   const document = JSON.parse(
     (await readFile(join(process.cwd(), './doc/openapi.json'))).toString(
